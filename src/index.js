7749607a2c317cacfa5ca2acc8ae08b2fd58025c
janeAnezi <110386328+janeAnezi@users.noreply.github.com>
2022-10-09 01:03:06 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import Cars from "./practice";
+
+const [honda, tesla] = Cars;
+const {
+  coloursByPopularity: [hondaTopColour],
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour],
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour </th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
